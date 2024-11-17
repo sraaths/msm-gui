6e5123ddb0827552105f16b31b3e44fe9d7d37b7
sraaths <sraaths@uchicago.edu>
2024-11-17 03:01:07 +0000

    c3

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,72 +1,55 @@
 class ActorsController < ApplicationController
-  def update
-    # Get the ID out of params
-    a_id = params.fetch("the_id")
-    # Look up the existing record
-    matching_records = Actor.where({ :id => a_id })
-    the_actor = matching_records.at(0)
-    # Overwrite each column with the values from user inputs
-    the_actor.name = params.fetch("the_name")
-    the_actor.dob = params.fetch("the_dob")
-    the_actor.bio = params.fetch("the_bio")
-    the_actor.image = params.fetch("the_image")
-    # Save
-    the_actor.save
-    # Redirect to the movie details page
-    redirect_to("/actors#{the_actor.id}")
-  end
-
-  def create
-    # params hash looks like this:
-    # {"the_title"=>"1", "the_year"=>"2", "the_duration"=>"3", "the_description"=>"4", "the_image"=>"5", "the_director_id"=>"6"}
-    a = Actor.new
-    a.name = params.fetch("the_name")
-a.dob = params.fetch("the_dob")
-a.bio = params.fetch("the_bio")
-a.image = params.fetch("the_image")
-a.save
-
-    a.save
-    redirect_to("/actors")
-    # Retrieve the user's inputs from params
-    # Create a record in the movie table
-    # Populate each column with the user input
-    # Save
-    # Redirect the user back to the /movies URL
-  end
-
- 
-
-
-  def destroy
-    # Fetch the ID from params
-    the_id = params.fetch("the_id")
-     
-    # Find the matching actor record(s)
-    matching_records = Actor.where({ :id => the_id })
-    the_actor = matching_records.at(0)
-     
-    # Delete the record if it exists
-    the_actor.destroy if the_actor
-     
-    # Redirect to the actors index
-    redirect_to("/actors")
-  end
-  
-  
   def index
-    matching_actors = Actor.all
-    @list_of_actors = matching_actors.order({ :created_at => :desc })
+    @list_of_actors = Actor.all.order({ :created_at => :desc })
 
     render({ :template => "actor_templates/index" })
   end
 
   def show
     the_id = params.fetch("path_id")
-
-    matching_actors = Actor.where({ :id => the_id })
-    @the_actor = matching_actors.at(0)
-      
+    @the_actor = Actor.where({:id => the_id }).at(0)
+    
     render({ :template => "actor_templates/show" })
+  end
+
+  def create
+    @the_actor = Actor.new
+    @the_actor.name = params.fetch("query_name")
+    @the_actor.dob = params.fetch("query_dob")
+    @the_actor.bio = params.fetch("query_bio")
+    @the_actor.image = params.fetch("query_image")
+
+    if @the_actor.valid?
+      @the_actor.save
+      redirect_to("/actors", { :notice => "Actor created successfully." })
+    else
+      redirect_to("/actors", { :notice => "Actor failed to create successfully." })
+    end
+  end
+
+  def update
+    the_id = params.fetch("path_id")
+    @the_actor = Actor.where({ :id => the_id }).at(0)
+
+    @the_actor.name = params.fetch("query_name")
+    @the_actor.dob = params.fetch("query_dob")
+    @the_actor.bio = params.fetch("query_bio")
+    @the_actor.image = params.fetch("query_image")
+
+    if @the_actor.valid?
+      @the_actor.save
+      redirect_to("/actors/#{@the_actor.id}", { :notice => "Actor updated successfully."} )
+    else
+      redirect_to("/actors/#{@the_actor.id}", { :alert => "Actor failed to update successfully." })
+    end
+  end
+
+  def destroy
+    the_id = params.fetch("path_id")
+    @the_actor = Actor.where({ :id => the_id }).at(0)
+
+    @the_actor.destroy
+
+    redirect_to("/actors", { :notice => "Actor deleted successfully."} )
   end
 end
--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,85 +1,74 @@
 class DirectorsController < ApplicationController
-  def update
-    # Get the ID out of params
-    d_id = params.fetch("the_id")
-    # Look up the existing record
-    matching_records = Director.where({ :id => d_id })
-    the_director = matching_records.at(0)
-    # Overwrite each column with the values from user inputs
-    the_director.name = params.fetch("the_name")
-    the_director.dob = params.fetch("the_dob")
-    the_director.bio = params.fetch("the_bio")
-    the_director.image = params.fetch("the_image")
-    # Save
-    the_director.save
-    # Redirect to the movie details page
-    redirect_to("/director#{the_director.id}")
-  end
-
-  def create
-    # params hash looks like this:
-    # {"the_title"=>"1", "the_year"=>"2", "the_duration"=>"3", "the_description"=>"4", "the_image"=>"5", "the_director_id"=>"6"}
-    d = Director.new
-    d.name = params.fetch("the_name")
-d.dob = params.fetch("the_dob")
-d.bio = params.fetch("the_bio")
-d.image = params.fetch("the_image")
-d.save
-
-    d.save
-    redirect_to("/directors")
-    # Retrieve the user's inputs from params
-    # Create a record in the movie table
-    # Populate each column with the user input
-    # Save
-    # Redirect the user back to the /movies URL
-  end
-
- 
-
-
-  def destroy
-    the_id = params.fetch("an_id")
-    matching_records = Director.where({ :id => the_id })
-    the_director = matching_records.at(0)
-    the_director.destroy
-    redirect_to("/directors")
-  end
-  
   def index
-    matching_directors = Director.all
-    @list_of_directors = matching_directors.order({ :created_at => :desc })
+    @list_of_directors = Director.all.order({ :created_at => :desc })
 
     render({ :template => "director_templates/index" })
   end
 
   def show
     the_id = params.fetch("path_id")
-
-    matching_directors = Director.where({ :id => the_id })
-    @the_director = matching_directors.at(0)
+    @the_director = Director.find(the_id)
 
     render({ :template => "director_templates/show" })
   end
 
+  def create
+    @the_director = Director.new
+    @the_director.name = params.fetch("query_name")
+    @the_director.dob = params.fetch("query_dob")
+    @the_director.bio = params.fetch("query_bio")
+    @the_director.image = params.fetch("query_image")
+
+    if @the_director.valid?
+      @the_director.save
+      redirect_to("/directors", { :notice => "Director created successfully." })
+    else
+      redirect_to("/directors", { :notice => "Director failed to create successfully." })
+    end
+  end
+
+  def update
+    the_id = params.fetch("path_id")
+    @the_director = Director.find(the_id)
+
+    @the_director.name = params.fetch("query_name")
+    @the_director.dob = params.fetch("query_dob")
+    @the_director.bio = params.fetch("query_bio")
+    @the_director.image = params.fetch("query_image")
+
+    if @the_director.valid?
+      @the_director.save
+      redirect_to("/directors/#{@the_director.id}", { :notice => "Director updated successfully."} )
+    else
+      redirect_to("/directors/#{@the_director.id}", { :alert => "Director failed to update successfully." })
+    end
+  end
+
+  def destroy
+    the_id = params.fetch("path_id")
+    @the_director = Director.find(the_id)
+
+    @the_director.destroy
+
+    redirect_to("/directors", { :notice => "Director deleted successfully."} )
+  end
+
   def max_dob
-    directors_by_dob_desc = Director.
+    @youngest = Director.
       all.
       where.not({ :dob => nil }).
-      order({ :dob => :desc })
-
-    @youngest = directors_by_dob_desc.at(0)
+      order({ :dob => :desc }).
+      at(0)
 
     render({ :template => "director_templates/youngest" })
   end
 
   def min_dob
-    directors_by_dob_asc = Director.
+    @eldest = Director.
       all.
       where.not({ :dob => nil }).
-      order({ :dob => :asc })
-      
-    @eldest = directors_by_dob_asc.at(0)
+      order({ :dob => :asc }).
+      at(0)
 
     render({ :template => "director_templates/eldest" })
   end
